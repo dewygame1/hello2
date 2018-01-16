a685a57c6a5e1c452bedbfae8007a5840d35462a
dewygame1@gmail.com <dewygame1@gmail.com>
2018-01-16 14:19:40 +0700

    hello W 10 times

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 	
 	public static void main (String[] args){
+		for(int i=0;i<10;i++){
 		System.out.printIn("Hello World");
 		// this project writed by java language
+		}
 	}
 }
