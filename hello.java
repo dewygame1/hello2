509c68a516dd2de5063cf2e41664b1f69890bfc0
dewygame1@gmail.com <dewygame1@gmail.com>
2018-01-16 14:42:42 +0700

    change

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 public class Hello{
 	
 	public static void main (String[] args){
-		//for(int i=0;i<10;i++){
+		for(int i=0;i<10;i++){
 		System.out.printIn("Hello earth");
 		// this project writed by java language
-		//}
+		}
 	}
 }
